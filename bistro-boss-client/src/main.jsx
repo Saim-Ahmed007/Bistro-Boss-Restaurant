ecbc4ef1412648c5fb68449f9c30cd9ee2875e11
Saim-Ahmed007 <saimahmed.seu.cse@gmail.com>
2025-06-16 16:32:00 +0600

    Fix react-helmet-async import issue

--- bistro-boss-client/src/main.jsx
+++ bistro-boss-client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
-import { HelmetProvider } from "react-helmet-async";
+
 import AuthProvider from "./provider/AuthProvider.jsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
@@ -13,11 +13,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
-        <HelmetProvider>
           <div className="max-w-screen-xl mx-auto">
             <RouterProvider router={router} />
           </div>
-        </HelmetProvider>
       </QueryClientProvider>
     </AuthProvider>
   </StrictMode>
